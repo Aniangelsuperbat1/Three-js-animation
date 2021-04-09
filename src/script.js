2502c50248044475509c3c813debe3b1f4686a4d
Patrick <zhen.patrick@gmail.com>
2021-04-09 19:58:54 -0400

    added nex texturesand background

--- src/script.js
+++ src/script.js
@@ -4,8 +4,15 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 
 // Textures
-const newTexture = new THREE.TextureLoader();
-const textureLoad = newTexture.load('/textures/NormalMap(2).jpg');
+const newTexture = new THREE.TextureLoader().load("/textures/2_no_clouds_4k.jpg");
+
+const newTexture2 = new THREE.TextureLoader().load("/textures/elev_bump_4k.jpg")
+
+const newTexture3 = new THREE.TextureLoader().load("/textures/water_4k.png")
+
+const newTexture4 = new THREE.TextureLoader().load("/textures/fair_clouds_4k.png")
+
+const newTexture5 = new THREE.TextureLoader().load("/textures/galaxy_starfield.png")
 
 // Debug
 const gui = new dat.GUI();
@@ -17,21 +24,63 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Objects
-const geometry = new THREE.SphereBufferGeometry(0.5, 64, 64);
+const geometry = new THREE.SphereBufferGeometry(1, 64, 64);
 
 // Materials
 
-const material = new THREE.MeshStandardMaterial();
-material.roughness = 0.2
-material.metalness = .1
+const material = new THREE.MeshPhongMaterial();
 material.color = new THREE.Color(0x292929);
-material.normalMap = textureLoad;
+material.bumpMap = newTexture2;
+material.map = newTexture;
+material.bumpScale = 0.1
+material.specularMap = newTexture3
+// material.specular = new THREE.Color("grey")
+material.map = newTexture4
+material.transparent = true
 
 // Mesh
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-// Light 1
+const newTexture1 = new THREE.TextureLoader();
+const textureLoad1 = newTexture1.load("/textures/2_no_clouds_4k.jpg");
+
+const geometry1 = new THREE.SphereBufferGeometry(0.5, 64, 64);
+
+// Materials
+
+const material1 = new THREE.MeshPhongMaterial();
+material1.color = new THREE.Color(0x292929);
+material1.map = newTexture;
+material1.bumpMap = newTexture2;
+material1.bumpScale = 0.04;
+material1.specularMap = newTexture3;
+// material1.specular = new THREE.Color("grey");
+
+// Mesh
+const sphere1 = new THREE.Mesh(geometry1, material1);
+sphere1.position.y = 0;
+sphere1.position.x = 0;
+sphere1.position.z = 0;
+
+scene.add(sphere1);
+
+gui.add(sphere1.position, "x").min(0).max(10).step(0.01);
+gui.add(sphere1.position, "y").min(0).max(10).step(0.01);
+gui.add(sphere1.position, "z").min(0).max(10).step(0.01);
+
+scene.background = newTexture5
+
+// const light = new THREE.AmbientLight(0x404040, 25); // soft white light
+// scene.add(light);
+
+scene.add(new THREE.AmbientLight(0x333333));
+
+var light = new THREE.DirectionalLight(0xffffff, 7);
+light.position.set(5, 3, 5);
+scene.add(light);
+
+//Light 1
 
 // const pointLight = new THREE.PointLight(0xffffff, 0.1);
 // pointLight.position.x = 2;
@@ -39,49 +88,50 @@ scene.add(sphere);
 // pointLight.position.z = 4;
 // scene.add(pointLight);
 
-
-
 //Light 2
-const newPointLight = new THREE.PointLight(0xff0000, 2);
-newPointLight.position.set(1,1,1)
-newPointLight.intensity = 10
-scene.add(newPointLight);
+// const newPointLight = new THREE.PointLight(0xffffff);
+// newPointLight.position.set(10, 50, 50);
+// newPointLight.intensity = 10;
+// // scene.add(newPointLight);
 
-const light2 = gui.addFolder("Light 2") 
+// const light2 = gui.addFolder("Light 2");
 
-light2.add(newPointLight.position, "x").min(-3).max(3).step(0.01);
-light2.add(newPointLight.position, "y").min(-6).max(6).step(0.01);
-light2.add(newPointLight.position, "z").min(-3).max(3).step(0.01);
-light2.add(newPointLight, "intensity").min(-6).max(10).step(0.01);
+// light2.add(newPointLight.position, "x").min(-3).max(3).step(0.01);
+// light2.add(newPointLight.position, "y").min(-6).max(6).step(0.01);
+// light2.add(newPointLight.position, "z").min(-3).max(3).step(0.01);
+// light2.add(newPointLight, "intensity").min(-6).max(10).step(0.01);
 
-const lightHelper = new THREE.PointLightHelper(newPointLight, 1)
-scene.add(lightHelper)
+// const light2Color = { color: 0xff0000 };
+// light2.addColor(light2Color, "color").onChange(() => {
+//   newPointLight.color.set(light2Color.color);
+// });
 
-// Light 3
+// const lightHelper = new THREE.PointLightHelper(newPointLight, 1);
+// scene.add(lightHelper);
 
-const newPointLight2 = new THREE.PointLight(0xff0000, 2);
-newPointLight2.position.set(1, 1, 1);
-newPointLight2.intensity = 10;
-scene.add(newPointLight2);
+// // Light 3
 
-const light3 = gui.addFolder("Light 3"); 
+// const newPointLight2 = new THREE.PointLight(0xff0000, 2);
+// newPointLight2.position.set(1, 1, 1);
+// newPointLight2.intensity = 10;
+// // scene.add(newPointLight2);
 
-light3.add(newPointLight2.position, "x").min(-3).max(3).step(0.01);
-light3.add(newPointLight2.position, "y").min(-6).max(6).step(0.01);
-light3.add(newPointLight2.position, "z").min(-3).max(3).step(0.01);
-light3.add(newPointLight, "intensity").min(0).max(10).step(0.01);
+// const light3 = gui.addFolder("Light 3");
 
-const light3Color = {color : 0xff0000}
-light3.addColor(light3Color, "color").onChange(() => {
-    newPointLight2.color.set(light3Color.color)
-})
+// light3.add(newPointLight2.position, "x").min(-3).max(3).step(0.01);
+// light3.add(newPointLight2.position, "y").min(-6).max(6).step(0.01);
+// light3.add(newPointLight2.position, "z").min(-3).max(3).step(0.01);
+// light3.add(newPointLight, "intensity").min(0).max(10).step(0.01);
 
+// const light3Color = { color: 0xff0000 };
+// light3.addColor(light3Color, "color").onChange(() => {
+//   newPointLight2.color.set(light3Color.color);
+// });
 
-const lightHelper2 = new THREE.PointLightHelper(newPointLight2, 1);
-scene.add(lightHelper2);
+// // Light helper
 
-// Light helper
-
+// const lightHelper2 = new THREE.PointLightHelper(newPointLight2, 1);
+// scene.add(lightHelper2);
 
 /**
  * Sizes
@@ -129,7 +179,7 @@ scene.add(camera);
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  alpha: true,
+  // alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -145,7 +195,10 @@ const animate = () => {
 
   // Update objects
   sphere.rotation.y = 0.5 * elapsedTime;
-  sphere.rotation.x = 0.5 * elapsedTime;
+  // sphere.rotation.x = 0.5 * elapsedTime;
+
+    sphere1.rotation.y = .05 * elapsedTime;
+    // sphere1.rotation.x = 1 * elapsedTime;
 
   // Update Orbital Controls
   // controls.update()
